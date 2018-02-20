63633bc468d77088bdce693b739ad2c99c29d406
dosarkz <ashenov.e@gmail.com>
2018-02-20 13:10:34 +0600

    Added file migration and model to image module

---
+++ src/Modules/Image/Database/Migrations/2016_08_21_071946_create_files_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('files', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('path');
+            $table->integer('user_id')->nullable();
+            $table->string('url')->nullable();
+            $table->integer('status_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('files');
+    }
+}
