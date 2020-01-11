990cec0946d6949e13a61a414b7220951cc4fb25
dosarkz <ashenov.e@gmail.com>
2020-01-11 17:12:16 +0600

    products items init

--- src/Installation/Modules/Lora/Resources/views/main/index.blade.php
+++ src/Installation/Modules/Lora/Resources/views/main/index.blade.php
@@ -32,18 +32,18 @@
             </div>
             <!-- /.col -->
 
-            <div class="col-12 col-sm-6 col-md-3">
-                <div class="info-box mb-3">
-                    <span class="info-box-icon bg-success elevation-1"><i class="fas fa-shopping-cart"></i></span>
+{{--            <div class="col-12 col-sm-6 col-md-3">--}}
+{{--                <div class="info-box mb-3">--}}
+{{--                    <span class="info-box-icon bg-success elevation-1"><i class="fas fa-shopping-cart"></i></span>--}}
 
-                    <div class="info-box-content">
-                        <span class="info-box-text">{{trans('lora::base.sales')}}</span>
-                        <span class="info-box-number">760</span>
-                    </div>
-                    <!-- /.info-box-content -->
-                </div>
-                <!-- /.info-box -->
-            </div>
+{{--                    <div class="info-box-content">--}}
+{{--                        <span class="info-box-text">{{trans('lora::base.sales')}}</span>--}}
+{{--                        <span class="info-box-number">760</span>--}}
+{{--                    </div>--}}
+{{--                    <!-- /.info-box-content -->--}}
+{{--                </div>--}}
+{{--                <!-- /.info-box -->--}}
+{{--            </div>--}}
 
             <div class="clearfix visible-sm-block"></div>
         </div>
